4f299d19ce9cd32cb10d6b3917b2dec2ca5a940e
LuisHenriqueJung <luis.cafu@outlook.com>
2022-06-21 13:10:56 -0300

    Finalização do segundo exercício e criação da classe ExercicioSet1

--- src/ExercicioList2.java
+++ src/ExercicioList2.java
@@ -1,9 +1,41 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ExercicioList2 {
     public static void main(String[] args) {
         List<String> respostas = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Telefonou para a vítima?");
+        respostas.add(sc.next().toLowerCase());
+        System.out.println("Esteve no local do crime?");
+        respostas.add(sc.next().toLowerCase());
+        System.out.println("Mora perto da vítima?");
+        respostas.add(sc.next().toLowerCase());
+        System.out.println("Devia para a vítima?");
+        respostas.add(sc.next().toLowerCase());
+        System.out.println("Já trabalhou com a vítima?");
+        respostas.add(sc.next().toLowerCase());
+
+        int count = 0;
+        Iterator<String> contador = respostas.iterator();
+        while (contador.hasNext()){
+            String resposta = contador.next();
+            if (resposta.equals("sim")){
+                count++;
+            }
+        }
+        switch (count){
+            case 2:
+                System.out.println(" VOCÊ FOI CLASSIFICADO COMO SUSPEITO "); break;
+            case 3:
+            case 4:
+                System.out.println("VOCÊ FOI CLASSIFICADO COMO CÚMPLICE "); break;
+            case 5:
+                System.out.println("VOCÊ FOI CLASSIFICADO COMO ASSASSINO "); break;
+            default:
+                System.out.println("VOCÊ FOI CLASSIFICADO COMO INOCENTE "); break;
+        }
+
     }
 }
 /*Utilizando listas, faça um programa que faça 5 perguntas para uma
---
+++ src/ExercicioSet1.java
@@ -0,0 +1,5 @@
+public class ExercicioSet1 {
+    public static void main(String[] args) {
+        
+    }
+}
